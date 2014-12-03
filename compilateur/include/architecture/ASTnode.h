2ae321a61b1c237b219b20f8c8699485ec92a976
vtc <vct@PCB120026.(none)>
2014-12-03 18:27:56 +0100

    nouvelle archi

---
+++ compilateur/include/architecture/ASTnode.h
@@ -0,0 +1,49 @@
+#ifndef ASTnode_h__
+# define ASTnode_h__
+
+/*
+ * Here is defined the majority of AST nodes
+ * implemntation are done in src
+ */
+
+namespace ctvscript {
+  namespace AST {
+    /*
+     * This interface will premit easier tree/list constructing
+     */
+    class node {};
+
+    /*
+     * The Factory help in two way: 
+		-> Parsing
+		-> [...]
+     */
+    
+
+    /*
+     * A key word is language specs
+     */
+    namespace keyword {
+      class Var		: AST::node{};
+      class Fun		: AST::node{};
+      class If		: AST::node{}; 
+      class Return	: AST::node{};
+    };
+
+    /*
+     * Symbols
+     */
+    namespace symbol {
+      class Semicolon	: AST::node{};
+      class WhiteSpace	: AST::node{};
+      class Bparenthesis: AST::node{};
+      class Eparenthesis: AST::node{};
+      class Bbracket	: AST::node{};
+      class Ebracket	: AST::node{};
+      class Bsqbrackets	: AST::node{};
+      class Esqbrackets	: AST::node{};
+    };
+  };
+};
+
+#endif
--- parsing/src/first.cpp
+++ parsing/src/first.cpp
@@ -1,18 +1,6 @@
-#include <fstream>
-
-#include "parsing/parser.hh"
+using dictionnary = char[256];
+    
+dictionnary whitespace;
 
 int main(int n, char** av) {
-  ctvscript::parser::language_parser py;
-  std::string file;
-  std::string line;
-
-  if (n > 1) {
-    std::fstream _file(av[1]);
-    do {
-      getline(_file, line);
-      file += line + "\n";
-    } while (!_file.eof());
-    py.parse(file);
-  }
 }
