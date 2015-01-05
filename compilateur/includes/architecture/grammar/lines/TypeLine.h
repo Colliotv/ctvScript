57cea0f069b1d184f9bb7838d36a8bcfb27dff56
CollioTV <collio_v@epitech.eu>
2015-01-05 08:49:12 +0100

    grammar commit

--- compilateur/includes/architecture/grammar/lines/TypeLine.h
+++ compilateur/includes/architecture/grammar/lines/TypeLine.h
@@ -15,43 +15,17 @@ class TypeDefinitionLine
 	  Match<AST::symbol::Star>
 	  >
 	,0 >,
-      Match<AST::symbol::Bparenthesis>,
-      Call<line_name::type_definition>,
-      Match<AST::symbol::Eparenthesis>,
       Repeat<
-	Or<
-	  And<
-	    Match<AST::symbol::Bsqbracket>,
-	    Match<AST::values::Integer>,
-	    Match<AST::symbol::Esqbracket>
-	    >,
-	  And<
-	    Match<AST::symbol::Bparenthesis>,
-	    Repeat<
-	      And<
-		Call<line_name::type_definition>,
-		Match<AST::symbol::Comma>
-		>
-	      ,0>,
-	    Repeat<
-	      Call<line_name::type_definition>
-	      ,0,1>,
-	    Match<AST::symbol::Eparenthesis>
-	    >
-	  >
-	, 0>
-      >,
-    And<
-      Repeat<
-	Or<
-	  Match<AST::values::Const>,
-	  Match<AST::symbol::Star>
-	  >
-	,0 >,
-      Repeat<
-	Match<AST::symbol::Reference>
+        Match<AST::symbol::Reference>
 	,0,1>,
-      Call<line_name::ScopedId>,
+      Or<
+        And<
+          Match<AST::symbol::Bparenthesis>,
+          Call<line_name::type_definition>,
+          Match<AST::symbol::Eparenthesis>
+         >,
+        Call<line_name::ScopedId>
+	>,
       Repeat<
 	Or<
 	  And<
